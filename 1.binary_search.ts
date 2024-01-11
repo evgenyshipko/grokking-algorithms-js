616b3cd73d519246cd3dd34212a99155bed2be60
Евгений Шипко <e.shipko@ritm.media>
2024-01-12 00:10:31 +0700

    initial commit

---
+++ 1.binary_search.ts
@@ -0,0 +1,38 @@
+// binary search
+// complexity log2(n)
+import { strict as assert } from 'node:assert';
+const binarySearch = (arr: number[], item: number) => {
+
+    let start = 0
+    let end = arr.length - 1
+
+    while (start <= end){
+
+        const middle = Math.floor((end + start) / 2)
+
+        // console.log('start', start, 'end', end, 'middle', middle)
+
+        const middleItem = arr[middle]
+
+        if (middleItem === item){
+            return middle
+        }
+        if (middleItem < item){
+            start = middle + 1
+        }
+        if (middleItem > item){
+            end = middle - 1
+        }
+    }
+    return null
+}
+
+assert.equal(binarySearch([1,10,100,1000,10000], 1) , 0)
+assert.equal(binarySearch([1,10,100,1000,10000], 10) , 1)
+assert.equal(binarySearch([1,10,100,1000,10000], 10000) , 4)
+assert.equal(binarySearch([1,10,100,1000,10000], 999) , null)
+assert.equal(binarySearch(Array.from({length: 100}, (_, i) => i + 1), 77), 76)
+assert.equal(binarySearch([], 99), null)
+
+
+
---
+++ README.md
@@ -0,0 +1,11 @@
+## Grokking Algorithms
+
+Here you can find implementation of algorithms from Aditya Bhargava "Grokking Algorithms" book on typescript.
+
+### Usage
+
+1. Install npm to your computer
+2. Install requirements
+``npm ci``
+3. Run algorithms using ts-node, e.g. 
+``ts-node 1.binary_search.ts``
